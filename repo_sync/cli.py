5c30397a96e7188ea9a27bb3dfe0851d3001a1f3
daiya <daimyjp2000@gmail.com>
2021-06-19 20:04:54 +0900

    add def cli

--- repo_sync/cli.py
+++ repo_sync/cli.py
@@ -3,13 +3,40 @@ Cli
 
 Copyright (C) 2021 Netpro Project RepoSync
 """
+import ipaddress
+import click
+
 import core  # pylint: disable=E1101
 
 
-def cli() -> None:
-    """
-    cli
-    """
+class IPAddressType(click.ParamType):
+    name = 'ipaddress'
+
+    def convert(self, value, param, ctx):
+        try:
+            return ipaddress.ip_address(value)
+        except ValueError:
+            message = '{value} is not a valid ip address'.format(value=value)
+            self.fail(message, param, ctx)
+
+
+IP_ADDRESS = IPAddressType()
+
+
+@click.command()
+@click.option('--address', '-a', type=IP_ADDRESS, multiple=True)
+@click.option('--file', '-f', type=click.Path())
+def cli(address, file) -> None:
+    for ip in address:
+        click.echo(ip)
+        # click.echo(type(ip))
+    try:
+        click.echo(file)
+        content = open(file)
+        click.echo(content.read())
+        content.close()
+    except FileNotFoundError:
+        print('error: Noting file!')
     core.fop("README.md", ".cache")  # pylint: disable=E1101
 
 
