d96686f22317b7a6444599c5e4609d4b6185a6c9
Yuto Watanabe <yuto.w51942@gmail.com>
2021-06-19 22:21:56 +0900

    allow tests

--- build.sh
+++ build.sh
@@ -2,13 +2,7 @@
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
 cd core
-# create .cargo/config
-if [ "$(uname)" == 'Darwin' ]; then
-    mkdir .cargo
-    printf "[target.x86_64-apple-darwin]\nrustflags = [\n  \"-C\", \"link-arg=-undefined\",\n  \"-C\", \"link-arg=dynamic_lookup\",\n]\n\n[target.aarch64-apple-darwin]\nrustflags = [\n  \"-C\", \"link-arg=-undefined\",\n  \"-C\", \"link-arg=dynamic_lookup\",\n]" $SHELL > .cargo/config
-fi
-
 cargo build --release
 
 cd ..
-mv $SCRIPT_DIR/core/target/release/libcore.dylib $SCRIPT_DIR/repo_sync/core.so
+mv $SCRIPT_DIR/core/target/release/libcore.dylib $SCRIPT_DIR/repo_sync/file_op/core.so
--- repo_sync/cli.py
+++ repo_sync/cli.py
@@ -3,42 +3,32 @@ Cli
 
 Copyright (C) 2021 Netpro Project RepoSync
 """
-import ipaddress
+import logging
+from typing import List
+
 import click
 
-import core  # pylint: disable=E1101
+from .file_op import fop
 
-
-class IPAddressType(click.ParamType):
-    name = 'ipaddress'
-
-    def convert(self, value, param, ctx):
-        try:
-            return ipaddress.ip_address(value)
-        except ValueError:
-            message = '{value} is not a valid ip address'.format(value=value)
-            self.fail(message, param, ctx)
-
-
-IP_ADDRESS = IPAddressType()
+logging.basicConfig()
+_LOG = logging.getLogger(__name__)
+_LOG.setLevel(logging.INFO)
 
 
 @click.command()
-@click.option('--address', '-a', type=IP_ADDRESS, multiple=True)
-@click.option('--file', '-f', type=click.Path())
-def cli(address, file) -> None:
-    for ip in address:
-        click.echo(ip)
-        # click.echo(type(ip))
-    try:
-        click.echo(file)
-        content = open(file)
-        click.echo(content.read())
-        content.close()
-    except FileNotFoundError:
-        print('error: Noting file!')
-    core.fop("README.md", ".cache")  # pylint: disable=E1101
+@click.option('--address', '-a', multiple=True, prompt=False,
+              help="Client address to synchronize.", required=True)
+@click.option('--file', '-f', 'file_path', type=click.Path(exists=True), prompt=True,
+              help="File path to synchronize.", required=True)
+def cli(address: List[str], file_path: str) -> None:
+    """
+    RepoSync cli
 
+    Args:
+        address (List[str]): client address to synchronize.
+        file_path (str): file path to synchronize.
+    """
+    _LOG.info("address: %s", ", \n".join(address))
+    _LOG.info("file %s", file_path)
 
-if __name__ == "__main__":
-    cli()
+    fop(file_path, '.cache')
