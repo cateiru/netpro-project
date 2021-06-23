5d403e5343857dd76012e25100c0c912576759b2
daiya <daimyjp2000@gmail.com>
2021-06-23 15:17:10 +0900

    hash output

--- repo_sync/git_log.py
+++ repo_sync/git_log.py
@@ -7,15 +7,31 @@ Copyright (C) 2021 Netpro Project RepoSync
 import click
 
 
-@click.command()
-@click.option('--repo-git', type=click.File('r'), prompt=True, help="view log", required=True)
-def git_log(repo_git: str) -> None:
+@click.group()
+def cli():
+    """
+    command grouping
+    """
+
+
+@cli.command()
+@click.option('--log', type=click.File('r'), prompt=True, help="view log", required=True)
+def git_log(log: str) -> None:
     """
     repo_git (str): read log.txt
     """
-    with open(repo_git) as file:
+    with open(log) as file:
         while True:
             line = file.readline()
             if not line:
                 break
             click.echo(line, nl=False)
+
+
+@cli.command()
+@click.option('--apply', type=str, prompt=True, help="show hash", required=True)
+def print_hash(apply: str) -> None:
+    """
+    apply (str): show hash
+    """
+    click.echo(apply)
