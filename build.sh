ba620dcc042e3001b898dfcbf7150007842c710b
Yuto Watanabe <yuto.w51942@gmail.com>
2021-07-06 12:15:17 +0900

    fixed build commands

--- build.sh
+++ build.sh
@@ -5,4 +5,14 @@ cd core
 cargo build --release
 
 cd ..
-mv $SCRIPT_DIR/core/target/release/libcore.dylib $SCRIPT_DIR/repo_sync/core.so
+
+if [ "$(uname)" == 'Darwin' ]; then
+    # MacOS
+    mv $SCRIPT_DIR/core/target/release/libcore.dylib $SCRIPT_DIR/repo_sync/core.so
+elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+    # Linux
+    mv $SCRIPT_DIR/core/target/release/libcore.so $SCRIPT_DIR/repo_sync/core.so
+else
+  echo "Your platform ($(uname -a)) is not supported."
+  exit 1
+fi
